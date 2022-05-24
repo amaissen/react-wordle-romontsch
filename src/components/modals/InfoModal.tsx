f2269e9320a8bdb3bf6aa24e2d785b3a83519a47
Alessandro Maissen <alessandro.maissen@bluewin.ch>
2022-05-24 18:31:08 +0200

    Make necessary changes for romontsch

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,64 +8,57 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Instrucziuns" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Emprova da lignar il plaid en 6 gadas. Suenter mintga emprova midan las
+        colurs e muossan con datier che ti eis dil plaid encuretg.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="T"
           status="correct"
         />
-        <Cell value="E" />
-        <Cell value="A" />
         <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="U" />
+        <Cell value="T" />
+        <Cell value="G" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        Il bustab T ei part dil plaid ed egl endretg liug.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
+        <Cell value="F" />
+        <Cell value="E" />
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="N"
           status="present"
         />
-        <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="A" />
+        <Cell value="R" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        Il bustab N ei part dil plaid, mo buca egl endretg liug.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
+        <Cell value="I" />
         <Cell value="E" />
+        <Cell isRevealing={true} isCompleted={true} value="R" status="absent" />
+        <Cell value="M" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        Il bustab R ei buca part dil plaid.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
-        <a
-          href="https://github.com/cwackerfuss/react-wordle"
-          className="underline font-bold"
-        >
-          check out the code here
-        </a>{' '}
+        Mess a disposizun dall'Uniun da Giuventetgna Sagogn.
       </p>
     </BaseModal>
   )
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,29 +1,29 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WIN_MESSAGES = ['Brigliant!', 'Magnific', 'Grondius!']
+export const GAME_COPIED_MESSAGE = 'Giug vegn copiaus egl archiv provisoric'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Buc avunda bustabs'
+export const WORD_NOT_FOUND_MESSAGE = 'Il plaid ei buc valeivels'
 export const HARD_MODE_ALERT_MESSAGE =
-  'Hard Mode can only be enabled at the start!'
+  'Il modus grev sa mo vegnir activaus all\'entschatta!'
 export const HARD_MODE_DESCRIPTION =
-  'Any revealed hints must be used in subsequent guesses'
-export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
+  'Mintga tip scuvretg sto vegnir druvaus ella proxima emprova.'
+export const HIGH_CONTRAST_MODE_DESCRIPTION = 'Per megliera veseivladad da colurs.'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+  `Il plaid encuretg ei ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `Must use ${guess} in position ${position}`
+  `Drova ${guess} en la posiziun ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
-export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
-export const SHARE_TEXT = 'Share'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+  `Emprova sto cumpigliar ${letter}`
+export const ENTER_TEXT = 'Confirmar'
+export const DELETE_TEXT = 'Stizzar'
+export const STATISTICS_TITLE = 'Statistica'
+export const GUESS_DISTRIBUTION_TEXT = 'Distribuziun d\'emprovas'
+export const NEW_WORD_TEXT = 'Niev plaid en'
+export const SHARE_TEXT = 'Parter'
+export const TOTAL_TRIES_TEXT = 'Emprovas'
+export const SUCCESS_RATE_TEXT = 'Quota da success'
+export const CURRENT_STREAK_TEXT = 'Seria da success'
+export const BEST_STREAK_TEXT = 'Pli liunga seria da success'
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
-  "You are using an embedded browser and may experience problems sharing or saving your results. We encourage you rather to use your device's default browser."
+  "Ei para che Vus druveis in navigatur pli special. Nus recumandein da druvar il navigatur da Vies appart."
